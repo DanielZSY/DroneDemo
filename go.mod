7a592084bcf56a0955e270355e4acce95ad54988
DanielZSY <danielzsy@126.com>
2022-11-24 19:18:01 +0800

    demo

---
+++ go.mod
@@ -0,0 +1,10 @@
+module drone
+
+go 1.19
+
+require github.com/zeromicro/go-zero v1.4.2
+
+require (
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"drone/config"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/zeromicro/go-zero/core/conf"
+)
+
+var configFile = flag.String("f", "user.yaml", "the config file")
 
 func main() {
 	fmt.Printf("Hello World!\n")
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	log.Printf("Starting userapi at %s:%d...\n", c.Host, c.Port)
 }
